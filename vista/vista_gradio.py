c0c9185dc891f7b62a49a96e4db796ce33fc33d9
Roberto Jimenez <robertojj2006@gmail.com>
2025-04-24 17:22:52 +0200

    correct errores

--- vista/vista_gradio.py
+++ vista/vista_gradio.py
@@ -35,20 +35,23 @@ class VistaGradio:
             with gr.Row():
                 nombre_estacion = gr.Textbox(label="Nombre de la Estación")
                 datos_registro = gr.Textbox(label="Registro de Datos")
+                salida_registro = gr.Textbox(label="Estado del Registro")
                 boton_registrar = gr.Button("Registrar Datos")
 
-            boton_registrar.click(fn=registrar_datos, inputs=[nombre_estacion, datos_registro], outputs="text")
+            boton_registrar.click(fn=registrar_datos, inputs=[nombre_estacion, datos_registro], outputs=[salida_registro])
 
             with gr.Row():
                 consulta_estacion = gr.Textbox(label="Consultar Estación")
+                salida_datos = gr.Textbox(label="Datos Registrados")
                 boton_consultar = gr.Button("Ver Datos")
 
-            boton_consultar.click(fn=obtener_datos, inputs=[consulta_estacion], outputs="text")
+            boton_consultar.click(fn=obtener_datos, inputs=[consulta_estacion], outputs=[salida_datos])
 
             with gr.Row():
                 nombre_guardar = gr.Textbox(label="Guardar Datos de la Estación")
+                salida_guardado = gr.Textbox(label="Estado del Guardado")
                 boton_guardar = gr.Button("Guardar en archivo.csv")
 
-            boton_guardar.click(fn=self.guardar_en_csv, inputs=[nombre_guardar], outputs="text")
+            boton_guardar.click(fn=self.guardar_en_csv, inputs=[nombre_guardar], outputs=[salida_guardado])
 
         interfaz.launch()
